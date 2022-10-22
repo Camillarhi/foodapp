ae3bc1c18194f9f8a47405a5e48b54435aa63a00
stepenchi <70510212+StephenChi-hi@users.noreply.github.com>
2022-10-22 14:42:47 +0100

    Chisom v2 (#5)
    * nav bar and banner fix 1
    
    * again1
    
    * fix nav and banner 2
    
    Co-authored-by: Rita Anene <92169163+Camillarhi@users.noreply.github.com>

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import banner_icon from "../images/banner_icon.png";
 import app_store from "../images/app_store.png";
-import google_play from "../images/google_play.png"
+import google_play from "../images/google_play.png";
 
 function Banner() {
   return (
@@ -10,7 +10,7 @@ function Banner() {
       <div className="row" style={{ paddingTop: 30, alignItems: "center" }}>
         <div className="col-xs-6 col-sm-8">
           <div>
-            <h1 style={{ fontSize: 50, paddingBottom: 30, }}>
+            <h1 style={{ fontSize: 50, paddingBottom: 30 }}>
               Oder <span style={{ color: "#e2b887" }}>food </span>any time
               anytime, <br></br> anywher
             </h1>
@@ -21,16 +21,24 @@ function Banner() {
               <br></br> delivered to you on time. Affordable, tasty and fast!
             </h3>
           </div>
-          <div className="row" style={{ paddingTop: 30, }}>
-            <img className="col-xs-6 col-sm-3" src={app_store} alt={"play-store"} />
-            <img className="col-xs-6 col-sm-3" src={google_play} alt={"app-staor"} />
+          <div className="row" style={{ paddingTop: 30 }}>
+            <img
+              className="col-xs-6 col-sm-3"
+              src={app_store}
+              alt={"play-store"}
+            />
+            <img
+              className="col-xs-6 col-sm-3"
+              src={google_play}
+              alt={"app-staor"}
+            />
           </div>
         </div>
-        <div className="col-xs-6 col-sm-3">
+        <div className="col-xs-6 col-sm-4">
           <img
             style={{
-              width: 470,
-              height: 400,
+              width: "100%",
+              height: 350,
               paddingBottom: 30,
               paddingTop: 30,
             }}
